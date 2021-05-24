ed4cec4c5702300edb631f73856ed86e6292c2e8
Nicole Coonen <83429588+NicoleLCoonen@users.noreply.github.com>
2021-05-24 17:22:03 -0500

    Update reports.js
    Added some on-the-fly styling for the tables and to display the general report a little better.

--- DeviceNetwork/Private/reports.js
+++ DeviceNetwork/Private/reports.js
@@ -1,13 +1,11 @@
-
 var Handler = {
 	
 	fn: {
 		displayReport: function(){
-			var report = $("#display").attr("data-report-type");
-			 if(report === "undefined"){
+			let report = $("#display").attr("data-report-type");
+			 if(typeof(report) === "undefined"){
 				 report = "";
 			 }else if(report !== ""){
-				 console.log(report);
 				let showReport = "#" + report ;
 				$("#display").show();
 				$(showReport).show();
@@ -23,28 +21,43 @@ var Handler = {
 		},
 		
 		styleToGo: function(){
-			$(".overview:visible").css({
-				"display":"flex",
-				"flex-flow": "row wrap",
-				"height": "150px"
-			});
-			
-			
 		
-			
 			let shown = $('#display').children(':visible');
 				
 			if(shown.length !== 0){
 				let formH = $('#reportForm').outerHeight();
-					$('#display').css("top", formH);
-				/*let headingH = $('.heading').outerHeight();
+				$('#display').css({"top": formH});
+			};
+			
+			let	$table = $(".floor > table");
+			if($table.length > 0){
+				$(".overview").css({"height" : "5%"});
+				
+				$(".breakdown").css({
+					"display" : "flex",
+					"flex-flow" : "row wrap",
+					"justify-content" : "space-between"
+				});
 					
-					$('.overview').css("top", headingH);
-				let overviewH = $('.overview').outerHeight();
-					overviewH = outerHeight + headingH;
-					$('.breakdown').css("top", overviewH);*/
-					
-			}
+				$(".floor").css({"flex": "1 1 16.5%", "height" : "100%"});
+				$("th:first-child,th:nth-child(3),th:nth-child(4),th:last-child").hide();
+				$("td:first-child,td:nth-child(3),td:nth-child(4),td:last-child").hide();
+				
+			};
+		},
+		
+		orderFlexItems: function(){
+			 $(".overview").children("h3").remove();
+			let first = $(".generic:contains(Total)").css({"order" : "1"});
+			
+		},
+		
+		print: function(){
+			
+			$('#print').click(function(){
+				$('#display').find(':visible').addClass('print');
+				
+			});
 		}
 	},
 		
@@ -52,6 +65,8 @@ var Handler = {
 		this.fn.displayReport();
 		this.fn.changeFloor();
 		this.fn.styleToGo();
+		this.fn.orderFlexItems();
+		this.fn.print();
 	}
 	
 };
