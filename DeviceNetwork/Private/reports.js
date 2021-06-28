bbe52572ca1272ee9dafcf9be02d5de9eaae1901
Nicole Coonen <83429588+NicoleLCoonen@users.noreply.github.com>
2021-06-28 17:33:20 -0500

    Update reports.js
    Added some functions for handling a recall form. Editing forms should be right around the corner.

--- DeviceNetwork/Private/reports.js
+++ DeviceNetwork/Private/reports.js
@@ -14,7 +14,7 @@ var Handler = {
 		},
 		
 		changeFloor: function(){
-			$('header option').click( function(){
+			$('#floorSelect').change( function(){
 				let url = $(this).val();
 				window.location.assign(url);
 			});
@@ -44,19 +44,128 @@ var Handler = {
 				$("td:first-child,td:nth-child(3),td:nth-child(4),td:last-child").hide();
 				
 			};
+			
+			let graveyard = $('.graveyard table:visible');
+			
+			if(graveyard.length > 0){
+				$(".overview").css('display' , 'block');
+				$('.graveyard th:last-child').show();
+				let $recall = $('.graveyard td:last-child:contains(1)');
+				$recall.replaceWith("<td><button type='button' class='recall'>Recall</button><button type='button' class='edit'>Edit</button></td>");
+			};
 		},
 		
 		orderFlexItems: function(){
-			 $(".overview").children("h3").remove();
-			let first = $(".generic:contains(Total)").css({"order" : "1"});
-			
+			let generalReport = $('#general:visible');
+			if(generalReport.length > 0){
+				$(".overview").children("h3").remove(); 
+				let totals =  $(".generic:contains(Total)")
+				let notTotal = $('.overview > .generic:not(:contains(Total))');
+				for(let x = 0; x < totals.length; x++){
+					$(totals).css("order", 1);
+					let group = $(totals[x]).prevAll(".generic");
+					let section = [totals[x]];
+					for(let y = 0; y < group.length; y++){
+						$(group[y]).css("order", y+2);
+						section[y+1] = group[y];
+					}
+					$(section).wrapAll("<div class='overview'></div>")
+				}
+				
+			}
 		},
+		
+		recallDevice: function(){
+			$('.recall').on('click',function(){
+				let formHidden = $('#recallForm:hidden');
+				let formVisible = $('#recallForm:visible');
+				
+				if(formHidden.length === 1){
+					
+					let $row = $(this).closest('tr');
+					let id = $row.children('td:first-child').text();
+					let name = $row.children('td:nth-child(2)').text();
+					let noncap = $row.children('td:nth-child(3)').text();
+					let model = $row.children('td:nth-child(4)').text();
+					
+					$('#deviceID').val(id);
+					$('#deviceName').val(name);
+					$('#deviceNC').val(noncap);
+					$('#deviceModel').val(model);
+					$('#recallForm').val('true').show();
+					
+					$('#portFloor').change(function(){
+						let dataCap = $(this).val();
+						let portList = $.parseJSON(dataCap);
+						let ports = portList.Ports;
+						let radioSelect ='';
+						
+						for(i=0; i < ports.length; i++){
+							let portID = ports[i].ID;
+							let portName = ports[i].Port_Name;
+							
+							radioSelect += "<input type='radio' name='newPort' id='" + portID +"' value='" + portID +">";
+							radioSelect +=	"<label for='" + portID + "'>" + portName + "</label></br>";
+						};
+						
+						$('#portSelection p').after(radioSelect);
+						$('#portSelection').show();
+						
+					});
+				
+				
+					let officeCheck = name.indexOf('-O');
+						
+						if(officeCheck !== -1){
+							let staffCap = $('#officeSelection').attr('data-caption');
+							let staffList = $.parseJSON(staffCap);
+							let staff = staffList.Staff;
+							let staffSelect ='';
+							
+							for(i=0; i < staff.length; i++){
+								let staffID = staff[i].ID;
+								let staffName = staff[i].Name;
+								
+								staffSelect += "<input type='radio' name='newUser' id='" + staffID +"' value='" + staffID +">";
+								staffSelect +=	"<label for='" + staffID + "'>" + staffName + "</label></br>";
+							};
+						
+							$('#officeSelection p').after(staffSelect);
+							$('#officeSelection').show();
+						};
+						
+				}else if(formVisible.length === 1){
+				
+					$('.recall').off('click');
+				};
+			});
+		},
+		closeForm: function() {
+			$("#closeForm").click(function(){
+				$('#deviceID').val('');
+				$('#deviceName').val('');
+				$('#recallForm').val('false').hide();	
+			});
+		},				
 		
 		print: function(){
 			
 			$('#print').click(function(){
+				let portTables = $('#available:visible,#broken:visible');
+				if(portTables.length > 0){
+					$('.floor').removeAttr('style');
+					$('.floor').css({
+						"flex-flow": "none"
+					});
+					$(".floor h4").css({"width" : "100%"});
+				};
 				$('#display').find(':visible').addClass('print');
+				$('header, form, #print').addClass('noprint');
 				
+				window.print();
+				$(".print").removeClass("print");
+				$(".noprint").removeClass("noPrint");
+				Handler.fn.styleToGo();
 			});
 		}
 	},
@@ -66,6 +175,8 @@ var Handler = {
 		this.fn.changeFloor();
 		this.fn.styleToGo();
 		this.fn.orderFlexItems();
+		this.fn.recallDevice();
+		this.fn.closeForm();
 		this.fn.print();
 	}
 	
