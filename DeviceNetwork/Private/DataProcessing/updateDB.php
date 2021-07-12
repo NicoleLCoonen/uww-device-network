2393409215b3ec9ae2fede68d30831c1b41cf9c9
Nicole Coonen <83429588+NicoleLCoonen@users.noreply.github.com>
2021-07-12 17:33:43 -0500

    Update updateDB.php
    Added form processing for accessories, moving devices between floors( if that wasn't updated the last time I made changes here), and fixed that date formatting.

--- DeviceNetwork/Private/DataProcessing/updateDB.php
+++ DeviceNetwork/Private/DataProcessing/updateDB.php
@@ -1,33 +1,16 @@
 <?php
 	if(is_post_request()){
+		
+		// setting all variables from $_POST at the top for later use
 		$result = "";
-				
+		$postKeys = array_keys($_POST);				
 		$portID = $_POST['portID'];
-		$portName = $_POST['portName'];
+		$portName = trim($_POST['portName']);
 		$status = $_POST['status'];
-		$deviceName = $_POST['deviceName'];
-		$model = $_POST['model'];
-		$nonCap = $_POST['nonCap'];
+		$deviceName = trim($_POST['deviceName']);
+		$model = trim($_POST['model']);
+		$nonCap = trim($_POST['nonCap']);
 			
-		// If the form was submitted with a name but no ID#, find the port based on its name and grab the ID# that way.
-		// The main way this happens is if the port info is cleared with the "move" button.
-		// Allows moving devices between floors by typing (or copy+pasting) the port name in the required input field.
-		if($portName !== '' && !$portID){
-			$sql = "SELECT * FROM data_ports WHERE Port_Name='" . $portName . "'";
-						
-			$portSearch = mysqli_query($db, $sql);
-						
-			if(!$portSearch){
-				exit("We couldn't find that port. Please make sure you've entered the name correctly.");
-			};
-						
-			while($port = mysqli_fetch_assoc($portSearch)){
-				$portID = $port['ID'];
-			};
-						
-			unset($sql);	
-		};
-				
 		if(isset($_POST["phpDelete"])){
 			$delete = 1;
 		} else {
@@ -39,8 +22,12 @@
 		} else {
 			$broken = 0 ;
 		};
-				
-		if($delete == 0 && (isset($portID) && isset($portName))){
+		
+		// Begin data processing
+		// Start by updating port info.
+		
+		// If the form was submitted with a port ID, then the port name will also be set.
+		if($portID !== ''){
 			$sql = "UPDATE data_ports";
 			$sql .= " SET Port_Name='" . $portName . "'";
 			$sql .= ", Port_Status=" . $status;
@@ -53,26 +40,54 @@
 				$result .= "Port information could not be updated. " . $sql ;
 				$result .= mysqli_error($db) ;	
 			};
-					
-			if($_POST['deviceID'] !== '') {
-				$deviceID = $_POST['deviceID'];
 				
-				if($_POST['vendor'] !== '' && $_POST['vendorName'] !== ''){
-					$vendor = $_POST['vendor'];
-					$vendorName = $_POST['vendorName'];
-							
-					$sql = "UPDATE printers_and_scanners";
-					$sql .= " SET Vendor='" . $vendor . "'";
-					$sql .= ", Vendor_Name='" . $vendorName . "', ";
+		/* If the form was submitted with a port name but no ID#, find the port based on its name and grab the ID# that way.
+		   The only way this happens is if the original port info is cleared with the "move" button.
+		   Allows moving devices between floors by typing (or copy+pasting) the port name in the required input field.
+		   Still check that the port name isn't empty so the code doesn't break. If no name is provided, nothing will happen.*/
+		} else if($portName !== '' && $portID === ''){
+			$sql = "SELECT * FROM data_ports WHERE Port_Name='" . $portName . "'";
 					
-				} else {
-					$sql = "UPDATE computers SET";
-				};
+			$portSearch = mysqli_query($db, $sql);
 						
-				$sql .= " Connection=" . $portID ;
-				$sql .= " WHERE ID=" . $deviceID;
+			if(!$portSearch){
+				exit("We couldn't find that port. Please make sure you've entered the name correctly.");
+			};
+						
+			while($port = mysqli_fetch_assoc($portSearch)){
+				$portID = $port['ID'];
+			};
+						
+			unset($sql);	
+		};
+
+		// Move onto device info, if any was submitted. 
+		// If a device ID was submitted, device info is being updated, not added
+		if($_POST['deviceID'] !== '') {
+			$deviceID = $_POST['deviceID'];
+			
+			// Check for an indicators of the type of device. 
+			// First check for 3rd-Party info
+			if($_POST['vendor'] !== '' && $_POST['vendorName'] !== ''){
+				$vendor = $_POST['vendor'];
+				$vendorName = $_POST['vendorName'];
+						
+				$sql = "UPDATE printers_and_scanners";
+				$sql .= " SET Vendor='" . $vendor . "'";
+				$sql .= ", Vendor_Name='" . $vendorName . "', ";
 					
+			// Then check whether it's office equipment based on the suffix (-O for "Office"  vs. -L for "Lab")		
+			} else if(strpos($deviceName, "-O")) {
+				$sql = "UPDATE staff_computers SET";
 			} else {
+				$sql = "UPDATE computers SET";
+			};
+						
+			$sql .= " Connection=" . $portID ;
+			$sql .= " WHERE ID=" . $deviceID;
+		
+		// if the device ID was empty, a new device may be being added 	
+		} else {
 					
 				if($_POST['vendor'] !== '' && $_POST['vendorName'] !== ''){
 					$vendor = $_POST['vendor'];
@@ -82,6 +97,9 @@
 					$sql .= " SET Vendor='" . $vendor . "'";
 					$sql .= ", Vendor_Name='" . $vendorName ."'";
 					$sql .= ", Device_Name='" . h($deviceName);
+					
+				} else if(strpos($deviceName, "-O")) {
+					$sql = "INSERT INTO staff_computers SET";
 			
 				} else {
 					$sql = "INSERT INTO computers" ;
@@ -111,9 +129,10 @@
 							
 					$format1 = strpos($_POST['dateRemoved'], '-');
 					$format2 = strpos($_POST['dateRemoved'], '/');
-							
-					if($format1 === true){
+						
+					if($format1 !== false){
 						$date = explode("-", $_POST['dateRemoved']);
+						print_r($date);
 						$format3 = strlen($date[0]);
 						
 						if($format3 === 2){
@@ -126,8 +145,9 @@
 							$day = $date[2];
 						};
 								
-					}else if($format2 === true){
+					}else if($format2 !== false){
 						$date = explode("/", $_POST['dateRemoved']);
+						print_r($date);
 						$format3 = strlen($date[0]);
 						
 						if($format3 === 2){
@@ -142,8 +162,22 @@
 					};
 				$sql .= ", Date_Removed='" . $year . '-' . $month . '-' . $day . "'" ;	
 				$sql .= ", Sent_To='" . $_POST['sentTo'] . "'" ;
-				$sql .= ", Notes='" . $_POST['notes'] . "'" ;					
+				$sql .= ", Notes='" . $_POST['notes'] . "'" ;
+				
 		};
+		
+		foreach($postKeys as $key){
+			if(preg_match("/accessory\d\z/", $key)){
+				$accessoryID = substr($key, 9);
+				$sql = "UPDATE accessories SET Device_Connection=0 WHERE ID=" . $accessoryID ;
+				if(mysqli_query($db, $sql)) {
+					$result .= "Accessory information successfully updated." ;
+				} else {
+					$result .= "Accessory information could not be updated. " . $sql ;
+					$result .= mysqli_error($db);	
+				};
+			};
+		};	
 					
 		if(mysqli_query($db, $sql)) {
 			$result .= "Device information successfully updated." ;
@@ -151,8 +185,11 @@
 			$result .= "Device information could not be updated. " . $sql ;
 			$result .= mysqli_error($db);	
 		};			
-					
-		if($delete === 1 ) {
+			
+		/* Figure out a better way to do this. Records should only
+		 be deleted ONLY once the data has been successfully moved to 
+		 the graveyard.*/
+		/*if($delete === 1 ) {
 			unset($sql);
 			$deviceID = $_POST['deviceID'];
 					
@@ -174,7 +211,7 @@
 				$result .= mysqli_error($db);	
 			};
 					
-		};
+		};*/
 											
 	};
 			
