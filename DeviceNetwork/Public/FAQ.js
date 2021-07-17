02fb82dcf9d84aa1a477ea1c8390cdd5c1561ace
Nicole Coonen <83429588+NicoleLCoonen@users.noreply.github.com>
2021-07-16 23:47:47 -0500

    Add files via upload
    Working on a FAQ page with instructions on how to use this tool

---
+++ DeviceNetwork/Public/FAQ.js
@@ -0,0 +1,33 @@
+var Handler = {
+	
+	fn: {
+		displayReport: function(){
+			$("#display").children().hide();
+			$('.topic button').click(function(){
+				let topic = $(this).attr("data-topic");
+				let showTopic= "#" + topic ;
+				$("#display").show();
+				let $show = $(showTopic).show();
+				$show.siblings().hide;
+			});
+		},
+		
+		changeFloor: function(){
+			$('#floorSelect').change( function(){
+				let url = $(this).val();
+				window.location.assign(url);
+			});
+		}
+	},
+	
+	init: function() {
+		this.fn.displayReport();
+		this.fn.changeFloor();
+	}
+	
+};
+
+$(function() {
+    Handler.init();
+
+});
