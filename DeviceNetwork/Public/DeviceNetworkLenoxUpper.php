31d6b23ebf49d53f85fe85b1b213bc0e35dd0180
Nicole Coonen <83429588+NicoleLCoonen@users.noreply.github.com>
2021-07-16 23:53:21 -0500

    Update DeviceNetworkLenoxUpper.php
    Copied in the wrong code!! Yikes.

--- DeviceNetwork/Public/DeviceNetworkLenoxUpper.php
+++ DeviceNetwork/Public/DeviceNetworkLenoxUpper.php
@@ -1,14 +1,12 @@
 <?php require_once('../Private/DataProcessing/placeholder.php');
-	 $thisFile = url_for('DeviceNetworkMain.php'); 
-	  
-?> 
-
+	 $thisFile = url_for('DeviceNetworkLenoxUpper.php'); 
+?>
 <!doctype html>
 <head>
-	<title>Andersen Device Network</title>
+	<title>Lenox Device Network</title>
 	<meta  name="viewport" content="width=device-width, initial-scale=1.0" >
 	<meta charset="utf-8" lang ="en-us">
-	<link rel='stylesheet' href='../Private/DNPStyling.css'>
+	<link rel='stylesheet' href='../Private/LenoxStyling.css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src='../Private/DNPsecured.js'> </script>
@@ -17,25 +15,19 @@
 
 <body>
 	<header class = 'masthead'> 
-		<h1>Andersen Library Device Network</h1>
-		<h2 id='floor'>Main Floor</h2>
+		<h1>Lenox Library Device Network</h1>
+		<h2 id='floor'>Upper Level</h2>
 		<span>
-			<button type='button' id='changeLibrary' data-url='<?php echo url_for("DeviceNetworkLenoxUpper.php");?>'>Lenox Maps</button>
-			<select id='floorSelect' name="floorSelect">
-				<option value="" disabled selected hidden>Change Floor</option>
-				<option value='<?php echo url_for("DeviceNetworkThird.php") ;?>'>Third Floor</option>
-				<option value='<?php echo url_for("DeviceNetworkFirst.php") ;?>'>First Floor</option>
-			</select>
+			<button type='button' id='changeLibrary' data-url='<?php echo url_for("DeviceNetworkMain.php");?>'>Andersen Maps</button>
+			<button type='button' id='changeFloor' data-url='<?php echo url_for("DeviceNetworkMLenoxLower.php");?>'>Lower Level</button>
 			<button type="button" id="reports" data-url='<?php echo url_for("reports.php");?>'>Run Reports</button>
 			<!--<button type="button" id="admin" data-url='<?php //echo url_for("admin.php");?>'>Admin</button>-->
 			<a href='<?php echo(url_for("FAQ.php"))?>'>FAQ</a>
 		</span>
-		
 	</header>
 
-	
 	<main>
-
+		
 			<div id="queries">
 				<input id="userInput" type='text' name="userInput" value="" placeholder="Search">
 				<button id="reset">Clear</button>
@@ -49,17 +41,11 @@
 				
 				require_once('updateForms.php');
 			?>
-		<!-- THIS IS FOR A LOWER PRIORITY FUNCTIONALITY THAT IS NOT COMPLETE 
-			<button type='button' id='editMarkers'>Add/Edit Markers</button>
-			<button type='submit' id='saveMarkers'>Done</button>
-		-->
-			<div id='image-wrapper' data-captions='<?php echo($mainJson) ; ?>'>
-				<img id='MainFloorBlueprint'class="map" src="../Private/MainBlueprintDNP-100.jpg" alt="A map of the Andersen Library's main floor, with markers indicating the locations of data ports" max-width="100%" height="auto" />
-				<caption for='MainFloorBlueprint'>For visual clarity, some ports have been grouped together or adjusted slightly from their physical location.</br>
-				Please consider marker loacations to be approximate.</caption>
+			<div id='image-wrapper' data-captions='<?php echo($lenoxUJson) ; ?>'>
+				<img id='blueprint'class="map" src="../Private/LenoxBlueprintUpper-100.jpg" alt="A map of the Lenox Library's main floor, with markers indicating the locations of data ports" max-width="100%" height="auto" />
 			</div>
 		</div>	
-			<?php 
+		<?php 
 				require_once('graveyard.php');
 				require("../Private/DataProcessing/updateDB.php");
 			?>
@@ -68,4 +54,3 @@
 	</main>
 	
 </body>
-
